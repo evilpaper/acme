c8c1017dab2bfaf5e569dcfb59bd9a2d545529ac
Pelle Lundgren <pellburt@gmail.com>
2024-01-18 22:02:29 +0100

    Add error and not found page

--- app/ui/invoices/edit-form.tsx
+++ app/ui/invoices/edit-form.tsx
@@ -118,7 +118,7 @@ export default function EditInvoiceForm({
         >
           Cancel
         </Link>
-        <Button type="submit">Edit Invoice</Button>
+        <Button type="submit">Save</Button>
       </div>
     </form>
   );
