171880cc52d07f5cd68e4e1c8e95e11516ca93eb
Pelle Lundgren <pellburt@gmail.com>
2024-01-24 08:08:52 +0100

    Add sign up button to dashboard

--- app/ui/login-form.tsx
+++ app/ui/login-form.tsx
@@ -82,7 +82,7 @@ function LoginButton() {
   const { pending } = useFormStatus();
   return (
     <Button className="mt-4 w-full" aria-disabled={pending}>
-      Log in <ArrowRightIcon className="ml-auto h-5 w-5 text-gray-50" />
+      Sign in <ArrowRightIcon className="ml-auto h-5 w-5 text-gray-50" />
     </Button>
   );
 }
