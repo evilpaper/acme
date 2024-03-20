83dfd40a476353c82a41dcc516c0dbeffb99d1a2
Pelle Lundgren <pellburt@gmail.com>
2024-03-20 21:15:56 +0100

    Add avatar

--- components/ui/main-nav.tsx
+++ components/ui/main-nav.tsx
@@ -5,6 +5,7 @@ import Link from 'next/link';
 import Image from 'next/image';
 import logo from '../../public/images/logo-black-lemon.svg';
 import { Icons } from './icons';
+import { Avatar, AvatarFallback, AvatarImage } from './avatar';
 
 export function MainNav() {
   const [showMobileMenu, setShowMobileMenu] = React.useState<boolean>(false);
@@ -14,7 +15,7 @@ export function MainNav() {
       <Link href="/home" className="hidden items-center space-x-2 md:flex">
         <Image priority src={logo} alt="ACME Fresh Produce Logo" height="48" />
       </Link>
-      <nav className="hidden gap-6 md:flex">
+      <nav className="hidden items-center gap-6 md:flex ">
         <Link
           href="/customers"
           className="flex items-center text-lg font-medium transition-colors hover:text-foreground/80 sm:text-sm"
@@ -27,6 +28,10 @@ export function MainNav() {
         >
           Invoices
         </Link>
+        <Avatar>
+          <AvatarImage src="logo" />
+          <AvatarFallback>CF</AvatarFallback>
+        </Avatar>
       </nav>
       <button
         className="flex items-center space-x-2 md:hidden"
