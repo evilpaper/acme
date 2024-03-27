ee981207915a68522cad35cdb5e7da7c823743fa
Pelle Lundgren <pellburt@gmail.com>
2024-03-27 06:39:24 +0100

    Move desktop signed in nav to aside

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -1,4 +1,5 @@
 import { MainNav } from '@/components/ui/main-nav';
+import Link from 'next/link';
 
 export default function HomePage() {
   return (
@@ -8,7 +9,20 @@ export default function HomePage() {
       </header>
       <div className="container grid flex-1 gap-12 md:grid-cols-[200px_1fr]">
         <aside className="hidden w-[200px] flex-col md:flex">
-          <p>I am aside</p>
+          <nav className="hidden flex-col items-start justify-between gap-6 md:flex">
+            <Link
+              href="/customers"
+              className="flex items-center text-lg font-medium transition-colors hover:text-foreground/80 sm:text-sm"
+            >
+              Customers
+            </Link>
+            <Link
+              href="/invoices"
+              className="flex items-center text-lg font-medium transition-colors hover:text-foreground/80 sm:text-sm"
+            >
+              Invoices
+            </Link>
+          </nav>
         </aside>
         <main className="flex w-full flex-1 flex-col overflow-hidden">
           <p>I am home</p>
