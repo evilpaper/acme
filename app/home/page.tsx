99b655e7030e243f70e9b14850ba869dccb8f6aa
Pelle Lundgren <pellburt@gmail.com>
2024-03-31 08:44:11 +0200

    Add stats card boilerplate

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -1,6 +1,33 @@
 import { MainNav } from '@/components/ui/main-nav';
 import Link from 'next/link';
 import { fraunces } from '../ui/fonts';
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from '@/components/ui/card';
+
+const widgets = [
+  {
+    title: 'Collected',
+    id: '123',
+  },
+  {
+    title: 'Pending',
+    id: '456',
+  },
+  {
+    title: 'Total invoces',
+    id: '789',
+  },
+  {
+    title: 'Total customers',
+    id: '987',
+  },
+];
 
 export default function HomePage() {
   return (
@@ -27,13 +54,29 @@ export default function HomePage() {
         </aside>
         <main className="flex w-full flex-1 flex-col overflow-hidden">
           <h1
-            className={`font-heading ${fraunces.className} text-3xl font-bold  leading-tight md:text-4xl`}
+            className={`font-heading ${fraunces.className} text-3xl font-bold leading-tight md:text-4xl`}
           >
             Home
           </h1>
-          <p className="text-lg text-muted-foreground">
+          <p
+            className={`py-4 text-lg text-muted-foreground ${fraunces.className}`}
+          >
             Monitor your performance
           </p>
+          <section className="grid gap-6 sm:grid-cols-2 lg:grid-cols-4">
+            {widgets.map(({ title, id }) => {
+              return (
+                <Card key={id}>
+                  <CardHeader>
+                    <CardTitle>{title}</CardTitle>
+                  </CardHeader>
+                  <CardContent>
+                    <p>Card Content</p>
+                  </CardContent>
+                </Card>
+              );
+            })}
+          </section>
         </main>
       </div>
     </div>
