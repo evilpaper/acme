0b3a331f8007aab70a7bea73b05678281c146423
Pelle Lundgren <pellburt@gmail.com>
2024-04-14 10:29:16 +0200

    Update url params on search to make it possible to get the value on the server

--- components/search.tsx
+++ components/search.tsx
@@ -2,23 +2,38 @@
 
 import React from 'react';
 import { Input } from './ui/input';
+import { usePathname, useSearchParams, useRouter } from 'next/navigation';
+import { useDebouncedCallback } from 'use-debounce';
 
 export function Search() {
   const [value, setValue] = React.useState('');
+  const searchParams = useSearchParams();
+  const pathname = usePathname();
+  const { replace } = useRouter();
 
-  const handleSearch = React.useCallback((term: string) => {
-    console.log(term);
-  }, []);
+  const handleSearch = React.useCallback(
+    (term: string) => {
+      console.log(term);
+      const params = new URLSearchParams(searchParams);
+      params.set('page', '1');
+      if (term) {
+        params.set('query', term);
+      } else {
+        params.delete('query');
+      }
+      replace(`${pathname}?${params.toString()}`);
+    },
+    [pathname, replace, searchParams],
+  );
 
   return (
     <Input
       type="search"
       placeholder="Search"
-      value={value}
       onChange={(event) => {
-        console.log(event.target.value);
-        setValue(event.target.value);
+        handleSearch(event.target.value);
       }}
+      defaultValue={searchParams.get('query')?.toString()}
     />
   );
 }
