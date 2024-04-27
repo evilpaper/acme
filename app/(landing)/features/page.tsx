eea198acc1a498c0dfe40be5d4203c8c6b9fa8b2
Pelle Lundgren <pellburt@gmail.com>
2024-04-27 08:20:58 +0200

    Update database provider

--- app/(landing)/features/page.tsx
+++ app/(landing)/features/page.tsx
@@ -42,13 +42,24 @@ export default function Page() {
         </div>
         <div className="relative overflow-hidden rounded-lg border bg-background p-2">
           <div className="flex h-[180px] flex-col justify-between rounded-md p-6">
-            <svg viewBox="0 0 24 24" className="h-12 w-12 fill-current">
-              <path d="M0 12C0 5.373 5.373 0 12 0c4.873 0 9.067 2.904 10.947 7.077l-15.87 15.87a11.981 11.981 0 0 1-1.935-1.099L14.99 12H12l-8.485 8.485A11.962 11.962 0 0 1 0 12Zm12.004 12L24 12.004C23.998 18.628 18.628 23.998 12.004 24Z" />
+            <svg
+              height="48"
+              stroke-linejoin="round"
+              viewBox="0 0 16 16"
+              width="48"
+              aria-label="Vercel logo"
+            >
+              <path
+                fill-rule="evenodd"
+                clip-rule="evenodd"
+                d="M8 1L16 15H0L8 1Z"
+                fill="currentColor"
+              ></path>
             </svg>
             <div className="space-y-2">
               <h3 className="font-bold">Database</h3>
               <p className="text-sm text-muted-foreground">
-                ORM using Prisma and deployed on PlanetScale.
+                Vercel Postgres deployed on Vercel
               </p>
             </div>
           </div>
