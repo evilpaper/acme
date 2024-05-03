d8bf1fdc6cd8005a142ba5fa77e0431127cb2dc1
Pelle Lundgren <pellburt@gmail.com>
2024-05-03 20:03:42 +0200

    Bring back old dashboard to compare

--- auth.config.ts
+++ auth.config.ts
@@ -8,7 +8,8 @@ export const authConfig = {
     authorized({ auth, request: { nextUrl } }) {
       const isLoggedIn = !!auth?.user;
       const isOnDashboard = nextUrl.pathname.startsWith('/dashboard');
-      if (isOnDashboard) {
+      const isOnOldDashboard = nextUrl.pathname.startsWith('/olddashboard');
+      if (isOnDashboard || isOnOldDashboard) {
         if (isLoggedIn) return true;
         return false; // Redirect unauthenticated users to login page
       } else if (isLoggedIn) {
