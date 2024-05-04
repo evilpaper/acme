70d9b41e82c6138776624bd32caf0de45515b07d
Pelle Lundgren <pellburt@gmail.com>
2024-05-04 07:25:15 +0200

    Move trash behind facade

--- components/delete-invoice-button.tsx
+++ components/delete-invoice-button.tsx
@@ -1,13 +1,13 @@
-import { Trash2 } from 'lucide-react';
 import { Button } from './ui/button';
 import { deleteInvoice } from '@/app/lib/actions';
+import { Icons } from './ui/icons';
 
 export default function DeleteInvoice({ id }: { id: string }) {
   const deleteInvoiceWithId = deleteInvoice.bind(null, id);
   return (
     <form action={deleteInvoiceWithId}>
       <Button variant="outline" size="icon">
-        <Trash2 className="h-4 w-4" />
+        <Icons.trashcan className="h-4 w-4" />
       </Button>
     </form>
   );
