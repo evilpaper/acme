18684802dcc556ff00e191c42a5e7ef85ea854cb
Pelle Lundgren <pellburt@gmail.com>
2024-06-02 20:03:31 +0200

    Fetch customers

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,4 +1,9 @@
+import { fetchCustomers } from '@/app/lib/data';
+
 export default async function Page() {
+  const customers = await fetchCustomers();
+
+  console.log(customers);
   return (
     <main className="container flex min-h-screen flex-col space-y-6 px-2">
       <h1
