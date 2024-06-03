b8a8f281d22d66fc91f5d6ed4df33d36c8598af9
Pelle Lundgren <pellburt@gmail.com>
2024-06-03 06:38:37 +0200

    Show customers in table

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,10 +1,26 @@
 import { fetchCustomers } from '@/app/lib/data';
 import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar';
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from '@/components/ui/card';
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from '@/components/ui/table';
+import Image from 'next/image';
 
 export default async function Page() {
   const customers = await fetchCustomers();
 
-  console.log(customers);
   return (
     <main className="container flex min-h-screen flex-col space-y-6 px-2">
       <h1
@@ -12,22 +28,39 @@ export default async function Page() {
       >
         Customers
       </h1>
-      <div className="space-y-8">
-        {customers.map(({ id, image_url, name, email }) => {
-          return (
-            <div className="flex items-center" key={id}>
-              <Avatar className="h-9 w-9">
-                <AvatarImage src={image_url} alt="Avatar" />
-                <AvatarFallback>OM</AvatarFallback>
-              </Avatar>
-              <div className="ml-4 space-y-1">
-                <p className="font-medium leading-none">{name}</p>
-                <p className="font-light text-muted-foreground">{email}</p>
-              </div>
-            </div>
-          );
-        })}
-      </div>
+      <section className="space-y-8">
+        <Table className="hidden md:table">
+          <TableHeader>
+            <TableRow>
+              <TableHead>Name</TableHead>
+              <TableHead>Email</TableHead>
+              <TableHead></TableHead>
+              <TableHead></TableHead>
+            </TableRow>
+          </TableHeader>
+          <TableBody>
+            {customers.map(({ id, image_url, name, email }) => {
+              return (
+                <TableRow key={id}>
+                  <TableCell className="flex items-center font-medium">
+                    <Image
+                      src={image_url}
+                      alt={`${name}'s profile picture`}
+                      className="mr-4 rounded-full"
+                      width={32}
+                      height={32}
+                    />
+                    {name}
+                  </TableCell>
+                  <TableCell>{email}</TableCell>
+                  <TableCell className="w-0  px-2"></TableCell>
+                  <TableCell className="w-0 px-2"></TableCell>
+                </TableRow>
+              );
+            })}
+          </TableBody>
+        </Table>
+      </section>
     </main>
   );
 }
