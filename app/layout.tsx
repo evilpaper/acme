1ef5fd147a577e9c059f6d4c07d1d895ccf62801
Pelle Lundgren <pellburt@gmail.com>
2024-06-27 21:16:19 +0200

    Add meta data to root layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,13 @@
 import '@/styles/global.css';
 import { fraunces } from '@/components/ui/fonts';
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'Acme',
+  description:
+    'An example app built using Next.js, Auth.js, Stripe, shadcn/ui and Fraunces typeface family.',
+  metadataBase: new URL('https://acme-seven-tau.vercel.app/'),
+};
 
 export default function RootLayout({
   children,
