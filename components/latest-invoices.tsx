9847f5ab0b75f0cb722ca729889b914cbbd8568b
Pelle Lundgren <pellburt@gmail.com>
2024-06-27 10:14:25 +0200

    Get initials from name for avatar fallback

--- components/latest-invoices.tsx
+++ components/latest-invoices.tsx
@@ -1,6 +1,23 @@
 import { fetchLatestInvoices } from '@/app/lib/data';
 import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar';
 
+function getInitials(name: string) {
+  if (!name) return '??';
+
+  let intials = [];
+
+  const characters = Array.from(name);
+
+  for (const character of characters) {
+    if (character === character.toUpperCase() && character !== ' ') {
+      intials.push(character);
+    }
+  }
+
+  // TODO: Add empty check
+  return intials.join('');
+}
+
 export async function LatestInvoices() {
   const latestInvoices = await fetchLatestInvoices();
 
@@ -11,7 +28,7 @@ export async function LatestInvoices() {
           <div className="flex items-center" key={invoice.id}>
             <Avatar className="h-9 w-9">
               <AvatarImage src={invoice.image_url} alt="Avatar" />
-              <AvatarFallback>OM</AvatarFallback>
+              <AvatarFallback>{getInitials(invoice.name)}</AvatarFallback>
             </Avatar>
             <div className="ml-4 space-y-1">
               <p className="font-medium leading-none">{invoice.name}</p>
