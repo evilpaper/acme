1b4a36f5d3ebc574d3c061d047ba7661a7ee0cbb
Pelle Lundgren <pellburt@gmail.com>
2024-06-28 08:15:36 +0200

    Resuse card component to get same higher on card skeleton as on card

--- components/dashboard/big-number-card.tsx
+++ components/dashboard/big-number-card.tsx
@@ -12,12 +12,11 @@ interface Props {
 export function BigNumberCard({ title, content, type }: Props) {
   return (
     <Card>
-      <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
+      <CardHeader className="pb-2">
         <CardTitle>{title}</CardTitle>
       </CardHeader>
       <CardContent>
         <div className="text-2xl font-bold">{content}</div>
-        <p className="py-1 text-muted-foreground">+?% from last month</p>
       </CardContent>
     </Card>
   );
--- components/dashboard/big-number-cards-skeleton.tsx
+++ components/dashboard/big-number-cards-skeleton.tsx
@@ -1,12 +1,49 @@
+import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card';
 import { Skeleton } from '@/components/ui/skeleton';
 
 export function BigNumberCardsSkeleton() {
   return (
     <>
-      <Skeleton className="h-[140px] w-full rounded-lg" />
-      <Skeleton className="h-[140px] w-full rounded-lg" />
-      <Skeleton className="h-[140px] w-full rounded-lg" />
-      <Skeleton className="h-[140px] w-full rounded-lg" />
+      <Card>
+        <CardHeader className="pb-2">
+          <CardTitle>
+            <Skeleton className="h-[1.5rem] rounded-lg" />
+          </CardTitle>
+        </CardHeader>
+        <CardContent>
+          <Skeleton className="h-[1.5rem] rounded-lg" />
+        </CardContent>
+      </Card>
+      <Card>
+        <CardHeader className="pb-2">
+          <CardTitle>
+            <Skeleton className="h-[1.5rem] rounded-lg" />
+          </CardTitle>
+        </CardHeader>
+        <CardContent>
+          <Skeleton className="h-[1.5rem] rounded-lg" />
+        </CardContent>
+      </Card>
+      <Card>
+        <CardHeader className="pb-2">
+          <CardTitle>
+            <Skeleton className="h-[1.5rem] rounded-lg" />
+          </CardTitle>
+        </CardHeader>
+        <CardContent>
+          <Skeleton className="h-[1.5rem] rounded-lg" />
+        </CardContent>
+      </Card>
+      <Card>
+        <CardHeader className="pb-2">
+          <CardTitle>
+            <Skeleton className="h-[1.5rem] rounded-lg" />
+          </CardTitle>
+        </CardHeader>
+        <CardContent>
+          <Skeleton className="h-[1.5rem] rounded-lg" />
+        </CardContent>
+      </Card>
     </>
   );
 }
