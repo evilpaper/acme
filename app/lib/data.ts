d126c5e2425def49755a612a8ba1f4672b85b2dd
Pelle Lundgren <pellburt@gmail.com>
2024-07-02 17:22:31 +0200

    Add todo for invoice id

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -148,6 +148,9 @@ export async function fetchInvoicesPages(query: string) {
 }
 
 export async function fetchInvoiceById(id: string) {
+  /**
+   * TODO: Narrow type for id and handle accordingly. id is a UUID. DB return with an error for all other types.
+   */
   noStore();
   try {
     const data = await sql<InvoiceForm>`
