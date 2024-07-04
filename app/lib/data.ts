359b24d17417f25a028a8f9fa496c7ce091e51de
Pelle Lundgren <pellburt@gmail.com>
2024-07-04 15:07:51 +0200

    Fix broken build, add back fetchInvoiceById

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -147,7 +147,7 @@ export async function fetchInvoicesPages(query: string) {
   }
 }
 
-export async function f(id: string) {
+export async function fetchInvoiceById(id: string) {
   noStore();
   try {
     const data = await sql<InvoiceForm>`
