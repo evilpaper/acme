1f6f6a6d4f7d8d0d2fbc6a388555fa3c5b16477d
Pelle Lundgren <pellburt@gmail.com>
2024-08-07 07:31:06 +0200

    Add markup for hamburger menu on signed out parts

--- app/(landing)/layout.tsx
+++ app/(landing)/layout.tsx
@@ -8,14 +8,26 @@ import logo from '../../public/images/logo-black-lemon.svg';
 import { useSelectedLayoutSegment } from 'next/navigation';
 import { cn } from '@/lib/utils';
 import { ModeSwitch } from '@/components/ui/mode-switch';
+import { AlignJustify } from 'lucide-react';
 
 export default function Page({ children }: { children: React.ReactNode }) {
   const segment = useSelectedLayoutSegment();
 
   return (
     <main className="container flex min-h-screen flex-col px-6">
-      <section className="flex justify-between py-6">
-        <nav className="flex items-center gap-6">
+      <nav className="flex items-center justify-between gap-6 py-6">
+        {/* Mobile */}
+        <AlignJustify className="md:hidden" />
+        <Link href="/" className="md:hidden">
+          <Image
+            priority
+            src={logo}
+            alt="ACME Fresh Produce Logo"
+            height="48"
+          />
+        </Link>
+        {/* Desktop */}
+        <section className="ml-6 hidden gap-10 md:flex">
           <Link href="/">
             <Image
               priority
@@ -24,37 +36,33 @@ export default function Page({ children }: { children: React.ReactNode }) {
               height="48"
             />
           </Link>
-          <section className="ml-6 hidden gap-10 md:flex">
-            <Link
-              href="/features"
-              className={cn(
-                'flex items-center p-2 text-lg font-medium transition-colors hover:text-foreground/80',
-                `/${segment}` === '/features'
-                  ? 'border-b border-black text-foreground'
-                  : 'border-b border-background text-foreground/80',
-              )}
-            >
-              Features
-            </Link>
-            <Link
-              href="/pricing"
-              className={cn(
-                'flex items-center p-2 text-lg font-medium transition-colors hover:text-foreground/80',
-                `/${segment}` === '/pricing'
-                  ? 'border-b border-black text-foreground'
-                  : 'border-b border-background text-foreground/80',
-              )}
-            >
-              Pricing
-            </Link>
-          </section>
-        </nav>
-        <nav>
-          <Button asChild>
-            <Link href="/login">Login</Link>
-          </Button>
-        </nav>
-      </section>
+          <Link
+            href="/features"
+            className={cn(
+              'flex items-center p-2 text-lg font-medium transition-colors hover:text-foreground/80',
+              `/${segment}` === '/features'
+                ? 'border-b border-black text-foreground'
+                : 'border-b border-background text-foreground/80',
+            )}
+          >
+            Features
+          </Link>
+          <Link
+            href="/pricing"
+            className={cn(
+              'flex items-center p-2 text-lg font-medium transition-colors hover:text-foreground/80',
+              `/${segment}` === '/pricing'
+                ? 'border-b border-black text-foreground'
+                : 'border-b border-background text-foreground/80',
+            )}
+          >
+            Pricing
+          </Link>
+        </section>
+        <Button asChild>
+          <Link href="/login">Login</Link>
+        </Button>
+      </nav>
       {children}
       <footer className="container mt-auto flex flex-col items-center justify-between p-6 md:flex-row">
         <p className="text-center text-sm">
