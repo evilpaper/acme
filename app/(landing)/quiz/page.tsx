8103df52c43845ec079458f8d21a74e89c2fdadc
Pelle Lundgren <pellburt@gmail.com>
2024-09-02 18:40:30 +0200

    Add quiz header

--- app/(landing)/quiz/page.tsx
+++ app/(landing)/quiz/page.tsx
@@ -1,3 +1,11 @@
 export default function Page() {
-  return <h1>Quiz</h1>;
+  return (
+    <section className="width-full mt-10 pb-8 pt-8">
+      <h1
+        className={`text-center text-5xl font-bold leading-tight tracking-tighter md:text-6xl`}
+      >
+        Quiz for devs
+      </h1>
+    </section>
+  );
 }
