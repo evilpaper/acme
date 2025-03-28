851fd9336eac25f847e6f5443004544b4302de8a
Pelle Lundgren <pellburt@gmail.com>
2025-03-28 17:20:07 +0100

    copy: move around copy for landing page

--- app/(landing)/page.tsx
+++ app/(landing)/page.tsx
@@ -6,17 +6,16 @@ export default function Page() {
     <section className="mt-10 flex grow flex-col gap-4 pb-8 pt-8">
       <div className="align-center flex flex-col items-center">
         <h1 className="mb-4 max-w-screen-md p-2 text-center text-4xl font-bold leading-tight tracking-tighter md:text-6xl">
-          Test your knowledge. Practice makes perfect. Or actually permanent but
+          Practice makes perfect. <br/> Or, actually, permanent... but
           you get it.
         </h1>
         <p className="mb-8 max-w-screen-sm text-center text-2xl">
-          An example app in the form of a quiz app for developers. Built using
-          Next.js, Auth.js, Stripe, shadcn/ui and Fraunces typeface family.
+          A quiz app for developers.
         </p>
-        <div className="flex flex-col flex-wrap gap-6 sm:flex-row">
+        <div className="mb-8 flex flex-col flex-wrap gap-6 sm:flex-row">
           <Button asChild>
             <Link href="/quiz/javascript" className="h-11 rounded-md px-8">
-              <span>Start random quiz</span>
+              <span>Try random quiz</span>
             </Link>
           </Button>
           <Button asChild variant="outline">
@@ -25,6 +24,9 @@ export default function Page() {
             </Link>
           </Button>
         </div>
+        <p className="max-w-screen-sm text-center text-xl">
+         <br/>Built as an example app using Next.js, Auth.js, Stripe, shadcn/ui and Fraunces typeface family.
+        </p>
       </div>
     </section>
   );
