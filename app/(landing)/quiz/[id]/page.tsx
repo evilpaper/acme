2ee9b1799bec77c705b07b58c7da688de6a9cb9e
Pelle Lundgren <pellburt@gmail.com>
2025-04-01 18:14:38 +0200

    feat: add dynamic routes to quizes

--- app/(landing)/quiz/[id]/page.tsx
+++ app/(landing)/quiz/[id]/page.tsx
@@ -1,18 +1,31 @@
 import Quiz from "@/components/quiz/quiz";
+import { Button } from "@/components/ui/button";
 import { javascriptQuestions } from "@/content/javascriptQuestions";
 import { shuffle } from "@/lib/utils";
+import Link from "next/link";
 
 export default async function Page({ params }: { params: { id: string } }) {
   const id = params.id;
 
-  // if (id === "javascript") {
-  //   return <Quiz quiz={javascriptQuestions} />;
-  // }
+  if (id === "javascript") {
+    const quiz = {
+      ...javascriptQuestions,
+      questions: shuffle(javascriptQuestions.questions),
+    };
+    return <Quiz quiz={quiz} />;
+  }
 
-  const quiz = {
-    ...javascriptQuestions,
-    questions: shuffle(javascriptQuestions.questions),
-  };
-
-  return <Quiz quiz={quiz} />;
+  return (
+    <div className="min-h-screen mt-12 flex flex-col items-center gap-6">
+      <p className="text-lg text-center">
+        Looks like you haven't selected a quiz. Or the quiz you are looking for
+        has moved.
+      </p>
+      <Button asChild>
+        <Link href="/quiz" className="flex h-11 w-fit gap-2 rounded-md px-8">
+          Back to Quizzes
+        </Link>
+      </Button>
+    </div>
+  );
 }
