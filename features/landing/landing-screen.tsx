f002355de90724923fe797b57f1ef902a65acfbd
Pelle Lundgren <pellburt@gmail.com>
2025-04-15 21:48:02 +0200

    fix: revert previous

--- features/landing/landing-screen.tsx
+++ features/landing/landing-screen.tsx
@@ -6,7 +6,8 @@ export function LandingScreen() {
     <section className="mt-1 md:mt-10 flex flex-col pb-8 pt-8">
       <div className="align-center flex flex-col items-center">
         <h1 className="mb-4 max-w-screen-md p-2 text-center text-4xl font-bold tracking-tighter md:text-6xl">
-          Hej Gustav! <br /> Or actually permanent... but you get it.
+          Practice makes perfect. <br /> Or actually permanent... but you get
+          it.
         </h1>
         <p className="mb-8 max-w-screen-sm text-center text-xl md:text-2xl">
           A quiz app for developers.
