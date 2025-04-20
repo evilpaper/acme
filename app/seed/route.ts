4488e94d417071602f0f36d2a7eb1f0aff661c5c
Pelle Lundgren <pellburt@gmail.com>
2025-04-20 08:29:18 +0200

    fix: temporary hide seed route to get pass production build

--- app/seed/route.ts
+++ app/seed/route.ts
@@ -1,57 +1,57 @@
-import bcrypt from "bcrypt";
-import { db } from "@vercel/postgres";
-import { users, quizzes } from "../lib/placeholder-data";
+// import bcrypt from "bcrypt";
+// import { db } from "@vercel/postgres";
+// import { users, quizzes } from "../lib/placeholder-data";
 
-const client = await db.connect();
+// const client = await db.connect();
 
-async function seedUsers() {
-  await client.sql`CREATE EXTENSION IF NOT EXISTS "uuid-ossp"`;
-  await client.sql`
-    CREATE TABLE IF NOT EXISTS users (
-      id UUID DEFAULT uuid_generate_v4() PRIMARY KEY,
-      name VARCHAR(255) NOT NULL,
-      email TEXT NOT NULL UNIQUE,
-      password TEXT NOT NULL
-    );
-  `;
+// async function seedUsers() {
+//   await client.sql`CREATE EXTENSION IF NOT EXISTS "uuid-ossp"`;
+//   await client.sql`
+//     CREATE TABLE IF NOT EXISTS users (
+//       id UUID DEFAULT uuid_generate_v4() PRIMARY KEY,
+//       name VARCHAR(255) NOT NULL,
+//       email TEXT NOT NULL UNIQUE,
+//       password TEXT NOT NULL
+//     );
+//   `;
 
-  const insertedUsers = await Promise.all(
-    users.map(async (user) => {
-      const hashedPassword = await bcrypt.hash(user.password, 10);
-      return client.sql`
-        INSERT INTO users (id, name, email, password)
-        VALUES (${user.id}, ${user.name}, ${user.email}, ${hashedPassword})
-        ON CONFLICT (id) DO NOTHING;
-      `;
-    }),
-  );
+//   const insertedUsers = await Promise.all(
+//     users.map(async (user) => {
+//       const hashedPassword = await bcrypt.hash(user.password, 10);
+//       return client.sql`
+//         INSERT INTO users (id, name, email, password)
+//         VALUES (${user.id}, ${user.name}, ${user.email}, ${hashedPassword})
+//         ON CONFLICT (id) DO NOTHING;
+//       `;
+//     }),
+//   );
 
-  return insertedUsers;
-}
+//   return insertedUsers;
+// }
 
-async function seedQuizzes() {
-  await client.sql`CREATE EXTENSION IF NOT EXISTS "uuid-ossp"`;
-  await client.sql`
-    CREATE TABLE IF NOT EXISTS quizzes (
-      id UUID DEFAULT uuid_generate_v4() PRIMARY KEY,
-      name VARCHAR(255) NOT NULL,
-      slug TEXT NOT NULL,
-      description TEXT NOT NULL
-    );
-  `;
+// async function seedQuizzes() {
+//   await client.sql`CREATE EXTENSION IF NOT EXISTS "uuid-ossp"`;
+//   await client.sql`
+//     CREATE TABLE IF NOT EXISTS quizzes (
+//       id UUID DEFAULT uuid_generate_v4() PRIMARY KEY,
+//       name VARCHAR(255) NOT NULL,
+//       slug TEXT NOT NULL,
+//       description TEXT NOT NULL
+//     );
+//   `;
 
-  const insertedQuizzes = await Promise.all(
-    quizzes.map(async (quiz) => {
-      return client.sql`
-        INSERT INTO quizzes (id, name, slug, description)
-        VALUES (${quiz.id}, ${quiz.name}, ${quiz.slug}, ${quiz.description})
-        ON CONFLICT (id) DO NOTHING;
-      `;
-    }),
-  );
+//   const insertedQuizzes = await Promise.all(
+//     quizzes.map(async (quiz) => {
+//       return client.sql`
+//         INSERT INTO quizzes (id, name, slug, description)
+//         VALUES (${quiz.id}, ${quiz.name}, ${quiz.slug}, ${quiz.description})
+//         ON CONFLICT (id) DO NOTHING;
+//       `;
+//     }),
+//   );
 
-  return insertedQuizzes;
-}
+//   return insertedQuizzes;
+// }
 
 export async function GET() {
   return Response.json({
