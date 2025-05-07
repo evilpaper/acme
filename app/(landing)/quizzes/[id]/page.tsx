7af50cee5275ffa730288f03015eba3ebe8cbd39
Pelle Lundgren <pellburt@gmail.com>
2025-05-07 20:43:49 +0200

    chore: clean up traces for static quizzes

--- app/(landing)/quizzes/[id]/page.tsx
+++ app/(landing)/quizzes/[id]/page.tsx
@@ -1,8 +1,6 @@
 import { shuffle } from "@/lib/utils";
 import Quiz from "@/features/quiz/quiz";
-import { lydiaHallieQuestions } from "@/content/lydiaHallie";
 import { QuizNotFound } from "@/features/quiz/quiz-not-found";
-import { javascriptQuestions } from "@/content/javascriptQuestions";
 import { getQuestionsByQuizId } from "@/features/quiz/quiz-data";
 
 export default async function Page({ params }: { params: { id: string } }) {
@@ -12,22 +10,6 @@ export default async function Page({ params }: { params: { id: string } }) {
 
   if (questions) {
     return <Quiz quiz={{ name: id, questions: questions }} />;
-  }
-
-  if (id === "javascript-101") {
-    const quiz = {
-      ...javascriptQuestions,
-      questions: shuffle(javascriptQuestions.questions),
-    };
-    return <Quiz quiz={quiz} />;
-  }
-
-  if (id === "lydia-hallies-javascript-questions") {
-    const quiz = {
-      ...lydiaHallieQuestions,
-      questions: shuffle(lydiaHallieQuestions.questions),
-    };
-    return <Quiz quiz={quiz} />;
   }
 
   return <QuizNotFound />;
