7c5bc1631df1ad6095a465e1e862a7207d8519aa
Pelle Lundgren <pellburt@gmail.com>
2025-06-05 06:36:50 +0200

    feat: add route for cards

---
+++ app/(landing)/cards/[slug]/page.tsx
@@ -0,0 +1,26 @@
+import NotFound from "./not-found";
+import Quiz from "@/features/quiz/quiz";
+import { randomizeQuestions } from "@/features/quiz/randomize-questions";
+import { getQuestionsByQuizId, getQuizBySlug } from "@/features/quiz/data/data";
+
+export default async function Page({ params }: { params: { slug: string } }) {
+  const slug = params.slug;
+  const quiz = await getQuizBySlug(slug);
+
+  if (!quiz || !Array.isArray(quiz) || quiz.length === 0) {
+    return <NotFound />;
+  }
+
+  const currentQuiz = quiz[0];
+  const questions = await getQuestionsByQuizId(currentQuiz.id);
+
+  const randomizedQuestions = randomizeQuestions(questions);
+
+  if (!questions || questions.length === 0) {
+    return <NotFound />;
+  }
+
+  return (
+    <Quiz quiz={{ name: currentQuiz.name, questions: randomizedQuestions }} />
+  );
+}
