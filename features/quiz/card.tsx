9f7e342c6ce79458445dd43df50268d96dbfd747
Pelle Lundgren <pellburt@gmail.com>
2025-06-22 09:55:45 +0200

    fix: temporary disable opacity to fix see through drag issue

--- features/quiz/card.tsx
+++ features/quiz/card.tsx
@@ -13,52 +13,43 @@ export function Card({ card }: Props) {
   const [selectedAnswer, setSelectedAnswer] = useState("");
 
   const x = useMotionValue(0);
-  const opacity = useTransform(x, [-150, 0, 150], [0, 1, 0]);
+  // const opacity = useTransform(x, [-150, 0, 150], [0, 1, 0]);
 
   const handleClick = () => {
     setIsFlipped(!isFlipped);
   };
 
   return (
-    <div
-      className="w-full h-full preserve-3d"
-      style={{
-        gridRow: 1,
-        gridColumn: 1,
-        transform: `rotate(${card.rotation}deg)`,
+    <motion.div
+      className="w-full h-full preserve-3d hover:cursor-grab active:cursor-grabbing"
+      style={{ gridRow: 1, gridColumn: 1, x, rotate: card.rotation }}
+      animate={{ rotateY: isFlipped ? 180 : 0 }}
+      transition={{
+        duration: 0.4,
+        type: "spring",
+        stiffness: 300,
+        damping: 20,
+      }}
+      drag="x"
+      dragConstraints={{
+        left: 0,
+        right: 0,
       }}
     >
-      <motion.div
-        className="w-full h-full preserve-3d hover:cursor-grab active:cursor-grabbing"
-        style={{ x, opacity }}
-        animate={{ rotateY: isFlipped ? 180 : 0 }}
-        transition={{
-          duration: 0.4,
-          type: "spring",
-          stiffness: 300,
-          damping: 20,
+      <CardBack
+        selectedAnswer={selectedAnswer}
+        correctanswer={card.correctanswer}
+        explanation={card.explanation}
+        source={card.source}
+      />
+      <CardFront
+        question={card}
+        handleAnswer={(answer) => {
+          setIsFlipped(true);
+          setSelectedAnswer(answer);
+          handleClick();
         }}
-        drag="x"
-        dragConstraints={{
-          left: 0,
-          right: 0,
-        }}
-      >
-        <CardFront
-          question={card}
-          handleAnswer={(answer) => {
-            setIsFlipped(true);
-            setSelectedAnswer(answer);
-            handleClick();
-          }}
-        />
-        <CardBack
-          selectedAnswer={selectedAnswer}
-          correctanswer={card.correctanswer}
-          explanation={card.explanation}
-          source={card.source}
-        />
-      </motion.div>
-    </div>
+      />
+    </motion.div>
   );
 }
