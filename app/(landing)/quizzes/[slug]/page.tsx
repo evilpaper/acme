670b11223ac13229b6c3c4a23f3d21eb317433e0
Pelle Lundgren <pellburt@gmail.com>
2025-06-28 09:51:49 +0200

    chore: remove randomize questions, add quizzes route

---
+++ app/(landing)/quizzes/[slug]/page.tsx
@@ -0,0 +1,17 @@
+export const metadata = {
+  title: "Quizzes | ACME",
+  description:
+    "Improve your knowledge with ACME's quizzes - Engage with our comprehensive collection of quizzes designed to enhance your learning experience.",
+  keywords:
+    "ACME quizzes, interactive quizzes, knowledge testing, educational quizzes, learning assessment",
+  openGraph: {
+    title: "Quizzes | ACME",
+    description:
+      "Solidify your knowledge with ACME's interactive quizzes - Engage with our comprehensive collection of quizzes designed to enhance your learning experience.",
+    type: "website",
+  },
+};
+
+export default async function Page() {
+  return <p>I am quizzes screen</p>;
+}
